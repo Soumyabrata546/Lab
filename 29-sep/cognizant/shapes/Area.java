a13b49ac371d0a7a1a0e420d5ba7415cfa187665
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-09-30 10:39:37 +0530

    Add files via upload

---
+++ 29-sep/cognizant/shapes/Area.java
@@ -0,0 +1,11 @@
+package com.cognizant.shapes;
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Circle c1=new Circle(2.5f);
+Circle c2=new Circle(3.0f,3.14f);
+	}
+
+}
