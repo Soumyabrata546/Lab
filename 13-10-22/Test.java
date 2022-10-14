10344872bf1d504fd4fef2decd7e970f4ffa36b8
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-10-14 11:41:32 +0530

    Add files via upload

---
+++ 13-10-22/Test.java
@@ -0,0 +1,12 @@
+package HandsOn_Lab;
+
+public class Test {
+private Test() {
+	System.out.println("Inside class possible");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Test t=new Test();
+	}
+
+}
