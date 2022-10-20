7a07858d6d73d34fae10fe7357ac3bfede8a87d9
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-10-20 13:44:31 +0530

    Update True_Odd.java

--- 20-10-22/True_Odd.java
+++ 20-10-22/True_Odd.java
@@ -8,16 +8,20 @@ Return true if every digit is odd otherwise false?
 public class True_Odd {
  boolean check(int d) {//method for checking every digit is odd or even
 	 int rem;
+	 if(d==0) {
+		 return false;
+	 }
 	 while(d!=0) {
-		 rem=d%10;
-		 if(rem%2==0)
+		 rem=d%10;//for getting the reminder
+		 if(rem%2==0)//checking either even or odd
 			 return false;
 		 d=d/10;
 	 }
 	 return true;
  }
- public static void main(String[] args) {
-	True_Odd ob=new True_Odd();
-	System.out.println(ob.check(1573));
+ public static void main(String[] args) {//main method
+	True_Odd ob=new True_Odd();//creating object
+	System.out.println(ob.check(0));
+	System.out.println(ob.check(355));
 }
 }
